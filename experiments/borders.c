0c27921d969fb43d692b113ce2fc038e2248402d
Alexandre Barret <barret.alx@gmail.com>
2025-05-10 17:00:17 +1200

    Draw borders on Termbox2 renderer

---
+++ experiments/borders.c
@@ -0,0 +1,72 @@
+#include <locale.h>
+
+#define CLAY_IMPLEMENTATION
+#define TB_IMPL
+#define TERMBOX_RENDERER_IMPL
+#include "../renderers/termbox2.h"
+
+int main() {
+  setlocale(LC_ALL, "");
+  uint64_t totalMemorySize = Clay_MinMemorySize();
+  Clay_Arena arena = Clay_CreateArenaWithCapacityAndMemory(
+      totalMemorySize, malloc(totalMemorySize));
+
+  tb_init();
+  int screenWidth = tb_width();
+  int screenHeight = tb_height();
+  Clay_Initialize(arena, (Clay_Dimensions){screenWidth, screenHeight},
+                  (Clay_ErrorHandler){HandleClayErrors});
+
+  Clay_SetMeasureTextFunction(MeasureTextTerminal, NULL);
+
+  struct tb_event ev;
+
+  while (1) {
+    Clay_SetLayoutDimensions(
+        (Clay_Dimensions){.width = tb_width(), .height = tb_height()});
+    tb_peek_event(&ev, 100);
+    if (ev.type == TB_EVENT_KEY && ev.ch == 0x0071) {
+      break;
+    }
+
+    Clay_BeginLayout();
+
+    // An example of laying out a UI with a fixed width sidebar and flexible
+    // width main content
+    CLAY({.id = CLAY_ID("root"),
+          .backgroundColor = COLOR_RED,
+          .layout = {.childGap = 1,
+                     .padding = CLAY_PADDING_ALL(1),
+                     .sizing = {CLAY_SIZING_GROW(), CLAY_SIZING_GROW()}}}) {
+      CLAY({.id = CLAY_ID("FIRST"),
+            .backgroundColor = COLOR_GREEN,
+            .border = {.width = CLAY_BORDER_ALL(3),
+                       .color = COLOR_WHITE,
+                       .backgroundColor = COLOR_GREEN},
+            .layout = {
+                .padding = CLAY_PADDING_ALL(1),
+                .sizing = {CLAY_SIZING_PERCENT(.5f), CLAY_SIZING_GROW()}}}) {
+        CLAY_TEXT(CLAY_STRING("Hello, World!"),
+                  CLAY_TEXT_CONFIG({.textColor = COLOR_WHITE,
+                                    .backgroundColor = COLOR_GREEN}));
+      }
+      CLAY({.id = CLAY_ID("SECOND"),
+            .backgroundColor = COLOR_BLUE,
+            .border = {.width = {.left = 1, .top = 2, .right = 3, .bottom = 4},
+                       .backgroundColor = COLOR_BLUE},
+            .layout = {
+                .padding = CLAY_PADDING_ALL(2),
+                .sizing = {CLAY_SIZING_PERCENT(.5f), CLAY_SIZING_GROW()}}}) {
+        CLAY_TEXT(CLAY_STRING("Hello, World!"),
+                  CLAY_TEXT_CONFIG({.textColor = COLOR_WHITE}));
+      }
+    }
+
+    // All clay layouts are declared between Clay_BeginLayout and Clay_EndLayout
+    Clay_RenderCommandArray renderCommands = Clay_EndLayout();
+    DrawClayCommands(renderCommands);
+    tb_present();
+  }
+  tb_shutdown();
+  return 0;
+}
--- renderers/termbox2.h
+++ renderers/termbox2.h
@@ -25,7 +25,8 @@ void HandleClayErrors(Clay_ErrorData errorData);
 uint16_t TBColorFromClayColor(const Clay_Color color);
 Clay_Dimensions MeasureTextTerminal(Clay_StringSlice text, Clay_TextElementConfig* cfg, void* userData);
 void DrawRectangle(const Clay_BoundingBox bbox, Clay_Color color);
-void DrawTextCommand(Clay_BoundingBox bbox, Clay_TextRenderData text);
+void DrawText(Clay_BoundingBox bbox, Clay_TextRenderData text);
+void DrawBorder(Clay_BoundingBox bbox, Clay_BorderRenderData text);
 void DrawClayCommands(Clay_RenderCommandArray commands);
 
 #ifdef TERMBOX_RENDERER_IMPL
@@ -91,15 +92,13 @@ void DrawRectangle(const Clay_BoundingBox bbox, Clay_Color color) {
   }
 }
 
-void DrawTextCommand(Clay_BoundingBox bbox, Clay_TextRenderData text) {
+void DrawText(Clay_BoundingBox bbox, Clay_TextRenderData text) {
   // Starting position
   int x = (int)bbox.x;
   int y = (int)bbox.y;
 
-  // Convert Clay’s RGBA textColor → TB_* (8‑color) fg attribute
   uint16_t fg = TBColorFromClayColor(text.textColor);
-  // We'll use the default background
-  const uint16_t bg = TB_BLACK;
+  uint16_t bg = TBColorFromClayColor(text.backgroundColor);
 
   // Iterate each rune in the slice
   const char* chars = text.stringContents.chars;
@@ -114,6 +113,78 @@ void DrawTextCommand(Clay_BoundingBox bbox, Clay_TextRenderData text) {
   }
 }
 
+typedef struct {
+    int tl, tr, bl, br, hb, vb;
+} BorderElements;
+
+static const BorderElements BORDER_SIZES[] = {
+    /* size 0 */ {  32,   32,   32,   32,   32,   32 },
+    /* size 1 */ {  43,   43,   43,   43,   45,  124 },
+    /* size 2 */ {9484, 9488, 9492, 9496, 9472, 9474 },
+    /* size 3 */ {9487, 9491, 9495, 9499, 9473, 9475 },
+    /* size 4 */ {9556, 9559, 9562, 9565, 9552, 9553 },
+};
+
+#define NUM_BORDER_SIZES \
+    (sizeof BORDER_SIZES / sizeof BORDER_SIZES[0])
+
+static inline BorderElements
+BorderElementsFromSize(int size)
+{
+    if ((unsigned)size >= NUM_BORDER_SIZES)
+        size = 0;
+    return BORDER_SIZES[size];
+}
+
+void DrawBorder(Clay_BoundingBox bbox, Clay_BorderRenderData border) {
+  // Starting position
+  int x0 = (int)bbox.x;
+  int y0 = (int)bbox.y;
+  int w = (int)bbox.width;
+  int h = (int)bbox.height;
+  int x1 = x0 + w - 1;
+  int y1 = y0 + h - 1;
+
+  uint16_t fg = TBColorFromClayColor(border.color);
+  uint16_t bg = TBColorFromClayColor(border.backgroundColor);
+
+  int leftSize = (int)border.width.left;
+  int topSize = (int)border.width.top;
+  int rightSize = (int)border.width.right;
+  int bottomSize = (int)border.width.bottom;
+
+  BorderElements leftStyle = BorderElementsFromSize(leftSize);
+  BorderElements topStyle = BorderElementsFromSize(topSize);
+  BorderElements rightStyle = BorderElementsFromSize(rightSize);
+  BorderElements bottomStyle = BorderElementsFromSize(bottomSize);
+
+  // if (leftSize+topSize > 1) {
+    tb_set_cell(x0, y0, topStyle.tl, fg, bg);
+  // }
+
+  // if (leftSize+bottomSize > 1) {
+    tb_set_cell(x1, y0, topStyle.tr, fg, bg);
+  // }
+
+  // if (rightSize+topSize > 1) {
+    tb_set_cell(x0, y1, bottomStyle.bl, fg, bg);
+  // }
+
+  // if (rightSize+bottomSize > 1) {
+    tb_set_cell(x1, y1, bottomStyle.br, fg, bg);
+  // }
+
+  for (int i = x0+1; i < x1; ++i) {
+    tb_set_cell(i, y0, topStyle.hb, fg, bg);
+    tb_set_cell(i, y1, bottomStyle.hb, fg, bg);
+  }
+
+  for (int j = y0+1; j < y1; ++j) {
+    tb_set_cell(x0, j, leftStyle.vb, fg, bg);
+    tb_set_cell(x1, j, rightStyle.vb, fg, bg);
+  }
+}
+
 void DrawClayCommands(Clay_RenderCommandArray commands) {
   for (int i = 0; i < commands.length; i++) {
     Clay_RenderCommand* renderCommand = &commands.internalArray[i];
@@ -124,15 +195,18 @@ void DrawClayCommands(Clay_RenderCommandArray commands) {
         break;
       }
       case CLAY_RENDER_COMMAND_TYPE_TEXT: {
-        DrawTextCommand(renderCommand->boundingBox,
+        DrawText(renderCommand->boundingBox,
                         renderCommand->renderData.text);
         break;
       }
-      case CLAY_RENDER_COMMAND_TYPE_NONE:
       case CLAY_RENDER_COMMAND_TYPE_BORDER:
+        DrawBorder(renderCommand->boundingBox,
+                          renderCommand->renderData.border);
+        break;
       case CLAY_RENDER_COMMAND_TYPE_IMAGE:
       case CLAY_RENDER_COMMAND_TYPE_SCISSOR_START:
       case CLAY_RENDER_COMMAND_TYPE_SCISSOR_END:
+      case CLAY_RENDER_COMMAND_TYPE_NONE:
       case CLAY_RENDER_COMMAND_TYPE_CUSTOM:
         break;
     }
