3bb121f8fb808719854aa5ec14abd604ddebc390
Alexandre Barret <barret.alx@gmail.com>
2025-05-27 10:24:11 +1200

    Add layout direction and child aligment enums

---
+++ mrblib/layout.rb
@@ -0,0 +1,41 @@
+module Clay
+  module LayoutDirection
+    DIRECTION_LEFT_TO_RIGHT = 0
+    DIRECTION_TOP_TO_BOTTOM = 1
+
+    def self.[](direction)
+      case direction
+      when :left_to_right then DIRECTION_LEFT_TO_RIGHT
+      when :top_to_bottom then DIRECTION_TOP_TO_BOTTOM
+      else                     DIRECTION_LEFT_TO_RIGHT
+      end
+    end
+  end
+#
+  module ChildAlignment
+    ALIGNMENT_LEFT = 0
+    ALIGNMENT_RIGHT = 1
+    ALIGNMENT_TOP = 0
+    ALIGNMENT_BOTTOM = 1
+    ALIGNMENT_CENTER = 2
+
+    X_MAPPING = {
+      left: ALIGNMENT_LEFT,
+      right: ALIGNMENT_RIGHT,
+      center: ALIGNMENT_CENTER,
+    }
+
+    Y_MAPPING = {
+      top: ALIGNMENT_TOP,
+      bottom: ALIGNMENT_BOTTOM,
+      center: ALIGNMENT_CENTER,
+    }
+
+    def self.[](x, y)
+      {
+        x: (X_MAPPING[x] || ALIGNMENT_LEFT),
+        y: (Y_MAPPING[y] || ALIGNMENT_TOP)
+      }
+    end
+  end
+end
---
+++ test/layout.rb
@@ -0,0 +1,23 @@
+class TestLayout < MTest::Unit::TestCase
+  def test_directions
+    assert_equal(0, Clay::LayoutDirection[:left_to_right])
+    assert_equal(1, Clay::LayoutDirection[:top_to_bottom])
+    assert_equal(0, Clay::LayoutDirection[:unknown])
+  end
+
+  def test_child_alignment
+    assert_equal({ x: 0, y: 0}, Clay::ChildAlignment[:unknown, :unknown])
+    assert_equal({ x: 0, y: 0}, Clay::ChildAlignment[:left, :top])
+    assert_equal({ x: 1, y: 0}, Clay::ChildAlignment[:right, :top])
+    assert_equal({ x: 2, y: 0}, Clay::ChildAlignment[:center, :top])
+    assert_equal({ x: 0, y: 1}, Clay::ChildAlignment[:left, :bottom])
+    assert_equal({ x: 1, y: 1}, Clay::ChildAlignment[:right, :bottom])
+    assert_equal({ x: 2, y: 1}, Clay::ChildAlignment[:center, :bottom])
+    assert_equal({ x: 0, y: 2}, Clay::ChildAlignment[:left, :center])
+    assert_equal({ x: 1, y: 2}, Clay::ChildAlignment[:right, :center])
+    assert_equal({ x: 2, y: 2}, Clay::ChildAlignment[:center, :center])
+  end
+
+end
+
+MTest::Unit.new.run
