f7a50236acb54247eda0f71792aaa758d1c0f4a9
Alexandre Barret <barret.alx@gmail.com>
2025-06-23 12:39:14 +1200

    Upsert white, and black colors

--- mrblib/colors.rb
+++ mrblib/colors.rb
@@ -21,15 +21,17 @@ module Clay
     end
 
     DEFAULT_COLORS = {
+      :black  => Color.new(0, 0, 0, 255),
       :red    => Color.new(255, 0, 0, 255),
       :green  => Color.new(0, 255, 0, 255),
       :blue   => Color.new(0, 0, 255, 255),
       :yellow => Color.new(255, 255, 0, 255),
       :cyan   => Color.new(0, 255, 255, 255),
-      :black  => Color.new(255, 255, 255, 255),
+      :white  => Color.new(255, 255, 255, 255),
     }
 
     # The syntax highlighting doesn't work but this is valid ruby in the latest versions.
+    def self.white  = DEFAULT_COLORS[:white]
     def self.red    = DEFAULT_COLORS[:red]
     def self.green  = DEFAULT_COLORS[:green]
     def self.blue   = DEFAULT_COLORS[:blue]
