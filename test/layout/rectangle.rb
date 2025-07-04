258ae6e211829c4c758101a8220d066894e73f89
Alexandre Barret <barret.alx@gmail.com>
2025-07-04 22:25:03 +1200

    Update basic rectangle test to support border commands

--- test/layout/rectangle.rb
+++ test/layout/rectangle.rb
@@ -15,6 +15,11 @@ class TestRectangle < MTest::Unit::TestCase
     Clay.ui(
       id: "root",
       background_color: Clay::Colors.red.to_h,
+      border: {
+        color: Clay::Colors.red.to_h,
+        background_color: Clay::Colors.blue.to_h,
+        width: { left: 1, right: 1, top: 1, bottom: 1, between_children: 1 }
+      },
       corner_radius: { top_left: 10, top_right: 10, bottom_left: 10, bottom_right: 10 },
       layout: {
         sizing: {
@@ -26,13 +31,22 @@ class TestRectangle < MTest::Unit::TestCase
 
     commands = Clay.end_layout
 
-    assert_equal([{
-      "type" => :rectangle,
-      "background_color" => { "r" => 255, "g" => 0, "b" => 0, "a" => 255 },
-      "bounding_box" => { "x" => 0.0, "y" => 0.0, "width" => 20.0, "height" => 20.0 },
-      "corner_radius" => { "top_left" => 10.0, "top_right" => 10.0, "bottom_left" => 10.0, "bottom_right" => 10.0 },
-    }], commands)
-
+    assert_equal([
+      {
+        "type" => :rectangle,
+        "bounding_box" => {"x" => 0.0, "y" => 0.0, "width" => 20.0, "height" => 20.0},
+        "background_color" => {"r" => 255.0, "g" => 0.0, "b" => 0.0, "a" => 255.0},
+        "corner_radius" => {"top_left" => 10.0, "top_right" => 10.0, "bottom_left" => 10.0, "bottom_right" => 10.0}
+      },
+      {
+        "type" => :border,
+        "bounding_box" => {"x" => 0.0, "y" => 0.0, "width" => 20.0, "height" => 20.0},
+        "color" => {"r" => 255.0, "g" => 0.0, "b" => 0.0, "a" => 255.0},
+        "background_color" => {"r" => 0.0, "g" => 0.0, "b" => 255.0, "a" => 255.0},
+        "corner_radius" => {"top_left" => 10.0, "top_right" => 10.0, "bottom_left" => 10.0, "bottom_right" => 10.0},
+        "width" => {"left" => 1, "right" => 1, "top" => 1, "bottom" => 1, "between_children" => 1}
+      }
+    ], commands)
   end
 end
 
